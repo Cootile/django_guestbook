2961ca8899a8b51df02dfcdd0227b735caab990b
lucas <anan21536@gmail.com>
2024-07-04 16:11:38 +0800

    auth, create, base.html added

--- message/urls.py
+++ message/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.MessageList.as_view(), name='msg_list') # message/ => 留言列表
+    path('', views.MessageList.as_view(), name='msg_list'),  # message/ => 留言列表
+    path('<int:pk>/', views.MessageRead.as_view(), name='msg_read'),
+    path('create/', views.MessageNew.as_view(), name='msg_create'),
 ]
--- message/views.py
+++ message/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import Message
 # Create your views here.
 
 class MessageList(ListView):
     model = Message
-
+    ordering = ['-id']
     #應用程式/資料模型_list.html
     #message/message_list.html
 
     #資料模型_list
+class MessageRead(DetailView):
+    model = Message
+
+class MessageNew(CreateView):
+    model = Message
+    fields = ['user', 'receipt', 'subject', 'content']
+    success_url = '/message/'
