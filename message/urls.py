9e9c8e6968a08429bab2ac965b9ac39982268d3a
lucas <anan21536@gmail.com>
2024-07-05 10:43:09 +0800

    logout, delete added

--- message/urls.py
+++ message/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.MessageList.as_view(), name='msg_list'),  # message/ => 留言列表
     path('<int:pk>/', views.MessageRead.as_view(), name='msg_read'),
     path('create/', views.MessageNew.as_view(), name='msg_create'),
+    path('<int:pk>/delete', views.MessageDelete.as_view(), name='msg_delete')
 ]
--- message/views.py
+++ message/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
-from .models import Message
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import *
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class MessageList(ListView):
     model = Message
-    ordering = ['-id']
+    ordering = ['-created']
     #應用程式/資料模型_list.html
     #message/message_list.html
 
@@ -16,4 +18,8 @@ class MessageRead(DetailView):
 class MessageNew(CreateView):
     model = Message
     fields = ['user', 'receipt', 'subject', 'content']
-    success_url = '/message/'
+    success_url = reverse_lazy('msg_list')
+
+class MessageDelete(LoginRequiredMixin, DeleteView):
+    model = Message
+    success_url = reverse_lazy('msg_list')
